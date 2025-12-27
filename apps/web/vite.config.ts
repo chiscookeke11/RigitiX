5fb721976a8afc9dfc893d74416df54e49939a33
chiscookeke11 <chiscookeke11@gmail.com>
2025-12-27 10:45:00 +0100

    progress

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -11,4 +11,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+   server: {
+    host: true, // 👈 important
+    port: 5173,
+    allowedHosts: [".ngrok-free.app"],
+  },
 })
